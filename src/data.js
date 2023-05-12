c6864333c62452057a8f3d551b971416a952181b
Egwuenu Precious <egwuenuprecious7@gmail.com>
2023-05-12 11:15:56 +0100

    feat: Added my project

--- src/data.js
+++ src/data.js
@@ -10,6 +10,17 @@ const data = [
       viewCode: "The link to the github should be here",
       borderColor: "#03EAE3"
     },
+    {
+      id: 2,
+      name: "Preshpie",
+      image: "https://res.cloudinary.com/dpokiomqq/image/upload/v1683728672/Screen_Shot_2023-05-10_at_3.22.39_PM_ks1mzx.png",
+      Technology: "HTML, CSS, JAVASCRIPT",
+      Time: "3 days",
+      Struggles: "I suffered, omo!",
+      viewLink: "Your hosted link should be here",
+      viewCode: "The link to the github should be here",
+      borderColor: "#fff"
+    },
 ]
 
 export default data;
